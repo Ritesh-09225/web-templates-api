d8d142b32b1fa7da7d353846cffcdf2ecbc71ca9
Ritesh-09225 <riteshvish94@gmail.com>
2026-06-07 20:29:44 +0530

    feat: add category-specific template files and implement a merge utility for template management

---
+++ merge.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const dataDir = path.join(__dirname, 'templates_data');
+const outputFile = path.join(__dirname, 'templates.json');
+
+const files = ['saas.json', 'local.json', 'portfolio.json', 'ecommerce.json', 'blog.json'];
+
+let allTemplates = [];
+
+for (const file of files) {
+  const filePath = path.join(dataDir, file);
+  if (fs.existsSync(filePath)) {
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    allTemplates = allTemplates.concat(data);
+  }
+}
+
+fs.writeFileSync(outputFile, JSON.stringify(allTemplates, null, 2));
+console.log(`Merged ${allTemplates.length} templates into templates.json`);
